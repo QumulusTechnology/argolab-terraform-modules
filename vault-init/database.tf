7cde725eefb8601d882eaffa2de85d326a966e3f
Daniel Niasoff <4070285+dniasoff@users.noreply.github.com>
2023-07-11 18:38:06 +0000

    temporarily disabled password autorotation for harbor database

--- vault-init/database.tf
+++ vault-init/database.tf
@@ -14,7 +14,7 @@ resource "vault_database_secret_backend_static_role" "harbor" {
   backend             = vault_database_secrets_mount.db.path
   db_name             = vault_database_secrets_mount.db.postgresql[0].name
   username            = "harbor"
-  rotation_period     = "3600"
+  rotation_period     = "31536000"
   rotation_statements = ["ALTER USER \"{{username}}\" WITH PASSWORD '{{password}}';"]
 }
 
