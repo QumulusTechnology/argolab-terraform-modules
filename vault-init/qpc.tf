2ca5e489ca764360d167c4f6ae092bee68e12391
Amine Bouabdallah <amine.bouabdallah@gmail.com>
2023-08-31 23:51:55 +0000

    Added kubernetes backend.db init

--- vault-init/qpc.tf
+++ vault-init/qpc.tf
@@ -27,7 +27,7 @@ resource "vault_azure_secret_backend_role" "qpc" {
 }
 
 resource "vault_policy" "qpc" {
-  name = "qpc-db-azure-access"
+  name = "qpc-database-azure-access"
 
   policy = <<EOT
 path "${vault_azure_secret_backend.this.path}/creds/${vault_azure_secret_backend_role.qpc.role}" {
@@ -77,10 +77,21 @@ resource "vault_database_secret_backend_role" "qpc_postgres" {
   default_ttl = 31536000
   max_ttl     = 31536000
   creation_statements = [
-    "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}';",
-    "GRANT ALL PRIVILEGES ON DATABASE ${vault_database_secrets_mount.this.postgresql[0].name} TO \"{{name}}\";",
-    "GRANT ALL ON ALL TABLES IN SCHEMA public TO \"{{name}}\";",
-    "GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO \"{{name}}\";",
-    "GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO \"{{name}}\";",
+    "CREATE ROLE \"{{name}}\" WITH LOGIN PASSWORD '{{password}}' VALID UNTIL '{{expiration}}' INHERIT;",
+    "GRANT qpc TO \"{{name}}\";",
+    "GRANT ALL ON DATABASE QPC TO QPC;",
+    "GRANT ALL PRIVILEGES ON SCHEMA PUBLIC TO QPC;",
+    "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA PUBLIC TO QPC;",
+    "GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA PUBLIC TO QPC;",
+    "GRANT ALL PRIVILEGES ON ALL FUNCTIONS IN SCHEMA PUBLIC TO QPC;",
   ]
+}
+
+resource "vault_kubernetes_auth_backend_role" "qpc" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "qpc-database-azure-access"
+  bound_service_account_names      = ["qpc-database-azure-credentials"]
+  bound_service_account_namespaces = ["qpc"]
+  token_ttl                        = 3600
+  token_policies                   = ["qpc-database-azure-access"]
 }
