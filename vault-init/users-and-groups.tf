7ce384bc497cd84ab32c9c6ac2748e03c4f3d9e8
Daniel Niasoff <4070285+dniasoff@users.noreply.github.com>
2024-03-31 10:33:18 +0000

    fixed issue with mixing up mount accessor with path

--- vault-init/users-and-groups.tf
+++ vault-init/users-and-groups.tf
@@ -1,15 +1,15 @@
 
 # This synchronises the groups from Azure AD into Vault
 module "vault_azure_ad_groups" {
-  source           = "git@github.com:QumulusTechnology/terraform-vault-azuread-groups.git?ref=v1.0.1"
-  auth_method      = vault_jwt_auth_backend.this.accessor
+  source           = "git@github.com:QumulusTechnology/terraform-vault-azuread-groups.git?ref=v1.0.2"
+  mount_accessor = vault_jwt_auth_backend.this.accessor
   security_enabled = true
 }
 
 # This synchronises the users from Azure AD into Vault
 module "vault_azure_ad_users" {
-  source      = "git@github.com:QumulusTechnology/terraform-vault-azuread-users.git?ref=v1.0.1"
-  auth_method = vault_jwt_auth_backend.this.accessor
+  source      = "git@github.com:QumulusTechnology/terraform-vault-azuread-users.git?ref=v1.0.2"
+  mount_accessor = vault_jwt_auth_backend.this.accessor
 }
 
 resource "vault_identity_group_policies" "vault_admins" {
