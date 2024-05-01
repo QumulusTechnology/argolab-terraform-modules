1ce8dfc3d0f622dfc216a71032f571a0c53925a3
jun christian villarosa <junchristianvillarosa@juns-MacBook-Pro-2.local>
2024-05-01 18:08:08 +0800

    added terraform

--- vault-init/data.tf
+++ vault-init/data.tf
@@ -97,18 +97,19 @@ data "kubernetes_secret" "azure-sso-credentials" {
   }
 }
 
-data "terraform_remote_state" "argolab" {
-  backend = "azurerm"
-  config = {
-    resource_group_name  = "global"
-    storage_account_name = "${var.global_storage_account_name}"
-    container_name       = "tfstate"
-    key                  = "${var.terraform_state_backend_key}/argolab"
-    subscription_id      = "${var.subscription_id}"
-    tenant_id            = "${var.tenant_id}"
-    use_oidc             = true
-  }
-}
+#Change to AWS
+# data "terraform_remote_state" "argolab" {
+#   backend = "azurerm"
+#   config = {
+#     resource_group_name  = "global"
+#     storage_account_name = "${var.global_storage_account_name}"
+#     container_name       = "tfstate"
+#     key                  = "${var.terraform_state_backend_key}/argolab"
+#     subscription_id      = "${var.subscription_id}"
+#     tenant_id            = "${var.tenant_id}"
+#     use_oidc             = true
+#   }
+# }
 
 data "external" "vault_init" {
   program = [
--- vault-init/database.tf
+++ vault-init/database.tf
@@ -12,13 +12,13 @@ resource "vault_database_secrets_mount" "db" {
     verify_connection = false
   }
 
-  postgresql {
-    name           = "harbor"
-    connection_url = "postgres://{{username}}:{{password}}@harbor-db-rw.harbor.svc:5432/harbor"
-    username       = data.kubernetes_secret.harbor_postgres_password.data["username"]
-    password       = data.kubernetes_secret.harbor_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "harbor"
+  #   connection_url = "postgres://{{username}}:{{password}}@harbor-db-rw.harbor.svc:5432/harbor"
+  #   username       = data.kubernetes_secret.harbor_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.harbor_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
   # postgresql {
   #   name           = "kamaji"
@@ -28,44 +28,44 @@ resource "vault_database_secrets_mount" "db" {
   #   allowed_roles  = ["*"]
   # }
 
-  postgresql {
-    name           = "keycloak"
-    connection_url = "postgres://{{username}}:{{password}}@keycloak-db-rw.keycloak.svc:5432/keycloak"
-    username       = data.kubernetes_secret.keycloak_postgres_password.data["username"]
-    password       = data.kubernetes_secret.keycloak_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "keycloak"
+  #   connection_url = "postgres://{{username}}:{{password}}@keycloak-db-rw.keycloak.svc:5432/keycloak"
+  #   username       = data.kubernetes_secret.keycloak_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.keycloak_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
-  postgresql {
-    name           = "nexus"
-    connection_url = "postgres://{{username}}:{{password}}@nexus-db-rw.nexus.svc:5432/nexus"
-    username       = data.kubernetes_secret.nexus_postgres_password.data["username"]
-    password       = data.kubernetes_secret.nexus_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "nexus"
+  #   connection_url = "postgres://{{username}}:{{password}}@nexus-db-rw.nexus.svc:5432/nexus"
+  #   username       = data.kubernetes_secret.nexus_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.nexus_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
-  postgresql {
-    name           = "pwpush"
-    connection_url = "postgres://{{username}}:{{password}}@pwpush-db-rw.pwpush.svc:5432/pwpush"
-    username       = data.kubernetes_secret.pwpush_postgres_password.data["username"]
-    password       = data.kubernetes_secret.pwpush_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "pwpush"
+  #   connection_url = "postgres://{{username}}:{{password}}@pwpush-db-rw.pwpush.svc:5432/pwpush"
+  #   username       = data.kubernetes_secret.pwpush_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.pwpush_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
-  postgresql {
-    name           = "semaphore"
-    connection_url = "postgres://{{username}}:{{password}}@semaphore-db-rw.semaphore.svc:5432/semaphore"
-    username       = data.kubernetes_secret.semaphore_postgres_password.data["username"]
-    password       = data.kubernetes_secret.semaphore_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "semaphore"
+  #   connection_url = "postgres://{{username}}:{{password}}@semaphore-db-rw.semaphore.svc:5432/semaphore"
+  #   username       = data.kubernetes_secret.semaphore_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.semaphore_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
-  postgresql {
-    name           = "temporal"
-    connection_url = "postgres://{{username}}:{{password}}@temporal-db-rw.temporal.svc:5432/temporal"
-    username       = data.kubernetes_secret.temporal_postgres_password.data["username"]
-    password       = data.kubernetes_secret.temporal_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "temporal"
+  #   connection_url = "postgres://{{username}}:{{password}}@temporal-db-rw.temporal.svc:5432/temporal"
+  #   username       = data.kubernetes_secret.temporal_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.temporal_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
 }
