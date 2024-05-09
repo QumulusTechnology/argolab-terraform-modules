c708599615bca3950b5eac01509a121be7bfe58b
jun christian villarosa <junchristianvillarosa@juns-MacBook-Pro-2.local>
2024-05-09 14:35:39 +0800

    Disable sso for now

--- vault-init/vault.tf
+++ vault-init/vault.tf
@@ -1,11 +1,11 @@
-resource "azurerm_key_vault_secret" "vault_token" {
-  name         = "vault-token"
-  value        = var.vault_token
-  key_vault_id = data.azurerm_key_vault.argo.id
-}
+# resource "azurerm_key_vault_secret" "vault_token" {
+#   name         = "vault-token"
+#   value        = var.vault_token
+#   key_vault_id = data.azurerm_key_vault.argo.id
+# }
 
-resource "azurerm_key_vault_secret" "vault_init_response" {
-  name         = "vault-init-response"
-  value        = var.vault_init_response
-  key_vault_id = data.azurerm_key_vault.argo.id
-}
+# resource "azurerm_key_vault_secret" "vault_init_response" {
+#   name         = "vault-init-response"
+#   value        = var.vault_init_response
+#   key_vault_id = data.azurerm_key_vault.argo.id
+# }
