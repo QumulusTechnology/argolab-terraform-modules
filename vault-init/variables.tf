0ea1ba6cb131a0c9201d023bcf428ad07729effc
jun christian villarosa <junchristianvillarosa@juns-MacBook-Pro-2.local>
2024-05-16 13:27:57 +0800

    disabled vault

--- vault-init/variables.tf
+++ vault-init/variables.tf
@@ -1,14 +1,14 @@
-variable "global_storage_account_name" {
-  type = string
-}
+# variable "global_storage_account_name" {
+#   type = string
+# }
 
-variable "subscription_id" {
-  type = string
-}
+# variable "subscription_id" {
+#   type = string
+# }
 
-variable "tenant_id" {
-  type = string
-}
+# variable "tenant_id" {
+#   type = string
+# }
 
 variable "terraform_state_backend_key" {
   type = string
@@ -18,9 +18,9 @@ variable "vault_token" {
   type = string
 }
 
-variable "vault_init_response" {
-  type = string
-}
+# variable "vault_init_response" {
+#   type = string
+# }
 
 variable "cloud_portal_domain_prod" {
   type    = string
