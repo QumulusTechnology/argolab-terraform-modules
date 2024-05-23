49ce88c18c26b4d4d90da4901ca4785cbdb43bc8
jun christian villarosa <junchristianvillarosa@juns-MacBook-Pro-2.local>
2024-05-23 16:02:22 +0800

    added keycloak and harbor db

--- vault-init/database.tf
+++ vault-init/database.tf
@@ -12,29 +12,29 @@ resource "vault_database_secrets_mount" "db" {
     verify_connection = false
   }
 
-  # postgresql {
-  #   name           = "harbor"
-  #   connection_url = "postgres://{{username}}:{{password}}@harbor-db-rw.harbor.svc:5432/harbor"
-  #   username       = data.kubernetes_secret.harbor_postgres_password.data["username"]
-  #   password       = data.kubernetes_secret.harbor_postgres_password.data["password"]
-  #   allowed_roles  = ["*"]
-  # }
+  postgresql {
+    name           = "harbor"
+    connection_url = "postgres://{{username}}:{{password}}@harbor-db-rw.harbor.svc:5432/harbor"
+    username       = data.kubernetes_secret.harbor_postgres_password.data["username"]
+    password       = data.kubernetes_secret.harbor_postgres_password.data["password"]
+    allowed_roles  = ["*"]
+  }
 
-  # postgresql {
-  #   name           = "kamaji"
-  #   connection_url = "postgres://{{username}}:{{password}}@kamaji-db-rw.kamaji.svc:5432/kamaji"
-  #   username       = data.kubernetes_secret.kamaji_postgres_password.data["username"]
-  #   password       = data.kubernetes_secret.kamaji_postgres_password.data["password"]
-  #   allowed_roles  = ["*"]
-  # }
+  postgresql {
+    name           = "kamaji"
+    connection_url = "postgres://{{username}}:{{password}}@kamaji-db-rw.kamaji.svc:5432/kamaji"
+    username       = data.kubernetes_secret.kamaji_postgres_password.data["username"]
+    password       = data.kubernetes_secret.kamaji_postgres_password.data["password"]
+    allowed_roles  = ["*"]
+  }
 
-  # postgresql {
-  #   name           = "keycloak"
-  #   connection_url = "postgres://{{username}}:{{password}}@keycloak-db-rw.keycloak.svc:5432/keycloak"
-  #   username       = data.kubernetes_secret.keycloak_postgres_password.data["username"]
-  #   password       = data.kubernetes_secret.keycloak_postgres_password.data["password"]
-  #   allowed_roles  = ["*"]
-  # }
+  postgresql {
+    name           = "keycloak"
+    connection_url = "postgres://{{username}}:{{password}}@keycloak-db-rw.keycloak.svc:5432/keycloak"
+    username       = data.kubernetes_secret.keycloak_postgres_password.data["username"]
+    password       = data.kubernetes_secret.keycloak_postgres_password.data["password"]
+    allowed_roles  = ["*"]
+  }
 
   # postgresql {
   #   name           = "nexus"
@@ -44,13 +44,13 @@ resource "vault_database_secrets_mount" "db" {
   #   allowed_roles  = ["*"]
   # }
 
-  # postgresql {
-  #   name           = "pwpush"
-  #   connection_url = "postgres://{{username}}:{{password}}@pwpush-db-rw.pwpush.svc:5432/pwpush"
-  #   username       = data.kubernetes_secret.pwpush_postgres_password.data["username"]
-  #   password       = data.kubernetes_secret.pwpush_postgres_password.data["password"]
-  #   allowed_roles  = ["*"]
-  # }
+  postgresql {
+    name           = "pwpush"
+    connection_url = "postgres://{{username}}:{{password}}@pwpush-db-rw.pwpush.svc:5432/pwpush"
+    username       = data.kubernetes_secret.pwpush_postgres_password.data["username"]
+    password       = data.kubernetes_secret.pwpush_postgres_password.data["password"]
+    allowed_roles  = ["*"]
+  }
 
   # postgresql {
   #   name           = "semaphore"
