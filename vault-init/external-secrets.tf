a00b6289cc85b5c1b919918be8b03226a789a5f6
jun christian villarosa <junchristianvillarosa@juns-MacBook-Pro-2.local>
2024-07-01 13:37:10 +0800

    feat: added external secrets

---
+++ vault-init/external-secrets.tf
@@ -0,0 +1,18 @@
+resource "vault_policy" "external-secrets" {
+  name = "external-secrets-access"
+
+  policy = <<EOT
+path "${vault_azure_secret_backend.this.path}/creds/${vault_azure_secret_backend_role.external-secrets.role}" {
+  capabilities = ["read", "create", "update", "delete"]
+}
+EOT
+}
+
+resource "vault_kubernetes_auth_backend_role" "external-secrets" {
+  backend                          = vault_auth_backend.kubernetes.path
+  role_name                        = "external-secrets-access"
+  bound_service_account_names      = ["external-secrets-credentials"]
+  bound_service_account_namespaces = ["external-secrets"]
+  token_ttl                        = 31536000
+  token_policies                   = ["external-secrets-azure-access"]
+}
