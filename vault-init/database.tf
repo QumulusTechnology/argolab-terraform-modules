47a3f505ffb9d55e59f4ee55443d22ce7b96ba9e
jun christian villarosa <junchristianvillarosa@juns-MacBook-Pro-2.local>
2024-11-25 18:43:26 +0800

    fix: disable keycloak and harbor

--- vault-init/database.tf
+++ vault-init/database.tf
@@ -12,13 +12,13 @@ resource "vault_database_secrets_mount" "db" {
   #   verify_connection = false
   # }
 
-  postgresql {
-    name           = "harbor"
-    connection_url = "postgres://{{username}}:{{password}}@harbor-db-rw.harbor.svc:5432/harbor"
-    username       = data.kubernetes_secret.harbor_postgres_password.data["username"]
-    password       = data.kubernetes_secret.harbor_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "harbor"
+  #   connection_url = "postgres://{{username}}:{{password}}@harbor-db-rw.harbor.svc:5432/harbor"
+  #   username       = data.kubernetes_secret.harbor_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.harbor_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
   # postgresql {
   #   name           = "kamaji"
@@ -28,13 +28,13 @@ resource "vault_database_secrets_mount" "db" {
   #   allowed_roles  = ["*"]
   # }
 
-  postgresql {
-    name           = "keycloak"
-    connection_url = "postgres://{{username}}:{{password}}@keycloak-db-rw.keycloak.svc:5432/keycloak"
-    username       = data.kubernetes_secret.keycloak_postgres_password.data["username"]
-    password       = data.kubernetes_secret.keycloak_postgres_password.data["password"]
-    allowed_roles  = ["*"]
-  }
+  # postgresql {
+  #   name           = "keycloak"
+  #   connection_url = "postgres://{{username}}:{{password}}@keycloak-db-rw.keycloak.svc:5432/keycloak"
+  #   username       = data.kubernetes_secret.keycloak_postgres_password.data["username"]
+  #   password       = data.kubernetes_secret.keycloak_postgres_password.data["password"]
+  #   allowed_roles  = ["*"]
+  # }
 
   # postgresql {
   #   name           = "nexus"
